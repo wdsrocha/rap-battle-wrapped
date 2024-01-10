71536c9910fa3395ce5f53af86b6d6f332ef1188
Wesley Rocha <wesleysr1997@gmail.com>
2024-01-09 21:58:03 -0400

    Set constants

--- types/constants.ts
+++ types/constants.ts
@@ -1,15 +1,15 @@
 import { z } from "zod";
-export const COMP_NAME = "MyComp";
+export const COMP_NAME = "RapBattleWrapped2023";
 
 export const CompositionProps = z.object({
   title: z.string(),
 });
 
 export const defaultMyCompProps: z.infer<typeof CompositionProps> = {
-  title: "Next.js and Remotion",
+  title: "Xavier",
 };
 
 export const DURATION_IN_FRAMES = 200;
-export const VIDEO_WIDTH = 1280;
-export const VIDEO_HEIGHT = 720;
+export const VIDEO_WIDTH = 1080;
+export const VIDEO_HEIGHT = 1920;
 export const VIDEO_FPS = 30;
