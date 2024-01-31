6cd3318f7ca283f35f659ff97784295f95a2b6f2
Wesley Rocha <wesleysr1997@gmail.com>
2024-01-30 20:32:15 -0400

    Improve Display presentation

--- remotion/Display.tsx
+++ remotion/Display.tsx
@@ -17,23 +17,15 @@ export function Display(props: DisplayProps) {
     props.keyframes || [{ delay: 0, from: 0, delta: 100 }],
   );
 
-  // const y = interpolate(
-  //   spring({
-  //     frame,
-  //     fps,
-  //     config: {
-  //       damping: 200,
-  //     },
-  //   }),
-  //   [0, 1],
-  //   [0, -100],
-  // );
-  return (
+  let hidden = props.keyframes && props.keyframes[0].delay > frame;
+
+  return hidden ? null : (
     <AbsoluteFill
-      className={props.className}
+      className={`px-4 ${props.className}`}
       style={{
         top: height / 2,
         transform: `translateY(${y}px)`,
+        textWrap: "balance",
       }}
     >
       {props.children}
